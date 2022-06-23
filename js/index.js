4ea164012acc3beb200369830d6b99a2622368c7
Strouqe <a.chervonenko@gmail.com>
2022-06-23 18:48:47 +0300

    test4

--- js/index.js
+++ js/index.js
@@ -7,9 +7,10 @@ button.addEventListener("click", fib);
 function fib()  {
 
 document.getElementById("spinner").classList.remove("d-none");
-
+document.getElementById("value").classList.remove("text-danger");
 if(document.getElementById("index").value > 50){
 
+  document.getElementById("value").classList.add("text-danger");
   document.getElementById("spinner").classList.add("d-none");
   document.getElementById("value").innerHTML = "Can’t be larger than 50";
 } else {
@@ -19,6 +20,7 @@ server = `http:localhost:5050/fibonacci/${number}`;
 fetch(server).then((response) => {
   if(!response.ok){
     document.getElementById("spinner").classList.add("d-none");
+    document.getElementById("value").classList.add("text-danger");
     document.getElementById("value").innerHTML = `Server Error: 42 is the meaning of life`;
   }
   response.json().then( data => { document.getElementById("value").innerHTML = data.result});
